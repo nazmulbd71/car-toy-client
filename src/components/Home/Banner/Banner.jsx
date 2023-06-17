789051a7160341a50aa0f8e40907733a3b00fe53
nazmulbd71 <mdnazmulhossainbd85@gmail.com>
2023-06-18 02:27:41 +0600

    banner reponsive complete

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -2,13 +2,15 @@ import banner from '../../../assets/images/banner.png'
 
 const Banner = () => {
     return (
-        <div className='w-full h-[550px] flex items-center justify-evenly pt-5'>
-            <div className='w-1/2'>
+
+        <div className='w-full banner h-[550px] md:flex md:items-center md:justify-evenly pt-5'>
+            <div className='w-full md:w-1/2'>
                 <h1 className='text-5xl text-[#606c38]'>Get behind the wheel, <br /> start the engine, and let's go!</h1>
                 <p className='my-6 tracking-wider'>Unleash Your Inner Speedster: Explore Our Epic Car Toy Collection! Ignite Adventure and Revolutionize Playtime with Premium Car Toys!</p>
+                <button className='px-5 py-3 rounded-lg text-white bg-[#606c38]'>Get Your Toys</button>
             </div>
-            <div>
-                <img src={banner} alt="" />
+            <div className='hidden md:block'>
+                <img className='img-banner max-w-screen-sm' src={banner} alt="" />
             </div>
         </div>
     );
